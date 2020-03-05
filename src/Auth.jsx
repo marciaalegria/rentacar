39c935fa19344e9822c880ae06d8dddf3a5e2b24
marciaalegria <ytasegundo@gmail.com>
2020-03-05 14:40:07 -0300

    reparando error de sintaxis auth

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -22,7 +22,7 @@ export default (props) => {
     }
     return (
         <div>
-            { {user, email}}
+            { user&& email}
             {
                 !user &&
             <div>
